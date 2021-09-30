e41f970977489e886ed479c02cdb9e6d5653209d
Siasurai <anastasiiasurai@Anastasiia-MacBook-Air.local>
2021-09-30 09:58:53 +0200

    add animation

--- script/script.js
+++ script/script.js
@@ -33,3 +33,30 @@ function showSlides (n) {
   	}
   slides[n].style.display = "flex";
 }
+
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+// function showIt(el_class) {
+//   var el = document.getElementsByClassName(el_class);
+//   el.scrollIntoView(true, {behavior: "smooth"});
+// }
+// showIt('main-container')
+
+
+  // var el = document.getElementsByClassName('main-container'); 
+  // el.scrollIntoView(true, {behavior: "smooth"});
+
+  
